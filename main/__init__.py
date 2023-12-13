88aaa8eb2b044c7e72cd96a195f835da5290ccba
madloyer <87596107+madloyer@users.noreply.github.com>
2023-12-13 21:22:12 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("22330771", default=None, cast=int)
-API_HASH = config("5961c742f33534c68418694e08a776f7", default=None)
-BOT_TOKEN = config("6787784929:AAE44K3JCBDbT2JPa7ONqLA7AvZVRQytnLw", default=None)
-SESSION = config("BQCK6qGNMTOYX3aUyJaGn6QZPX739G21afoW14VXjEeDVOw1IGqIWTgpfvo5jfRSURN1B7HPSWviMSus6iNj3SvPclJkieBFLZVjZ-uiR2v-fzz4duVWaf4mWuXgOJJ6gQl3oUE0qQogAPR1O7EEcFk91ydebtWacAzcSPddWr7Or-lCsOJEM0wqEdk2hBWfE9Vaf3HBeioJsKIB3TQ2j4HUgwMhASe_fUmDY5hmUHbxucStyK6v1HkpQMzWVNlgDKmWoXtSEt5EpzIAmGntKXnyGOJoy8QxiwgNt7bbXhdAbDFN1YLcduMROE-azEmhp-mLtopUf7AMG39hSsUUcs0UAAAAATaM_jsA", default=None)
-FORCESUB = config("tiktokviralbarue", default=None)
-AUTH = config("1281619082", default=None, cast=int)
+API_ID = "22330771"
+API_HASH = "5961c742f33534c68418694e08a776f7"
+BOT_TOKEN = "6787784929:AAE44K3JCBDbT2JPa7ONqLA7AvZVRQytnLw"
+SESSION = "BQCK6qGNMTOYX3aUyJaGn6QZPX739G21afoW14VXjEeDVOw1IGqIWTgpfvo5jfRSURN1B7HPSWviMSus6iNj3SvPclJkieBFLZVjZ-uiR2v-fzz4duVWaf4mWuXgOJJ6gQl3oUE0qQogAPR1O7EEcFk91ydebtWacAzcSPddWr7Or-lCsOJEM0wqEdk2hBWfE9Vaf3HBeioJsKIB3TQ2j4HUgwMhASe_fUmDY5hmUHbxucStyK6v1HkpQMzWVNlgDKmWoXtSEt5EpzIAmGntKXnyGOJoy8QxiwgNt7bbXhdAbDFN1YLcduMROE-azEmhp-mLtopUf7AMG39hSsUUcs0UAAAAATaM_jsA"
+FORCESUB = "DroneBots"
+AUTH = "1281619082"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
